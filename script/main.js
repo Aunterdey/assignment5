6906f9f74ba452369c7bda8e3dc46a599dcdddcb
Aunter63 <aunterdey2002@gmail.com>
2025-03-02 10:32:52 +0600

    first commit

---
+++ script/main.js
@@ -0,0 +1,16 @@
+document.getElementById("first-btn")
+.addEventListener("click",function(event){
+    event.preventDefault();
+ alert("Board update successfully")})
+
+ 
+  
+
+
+document.getElementById("Discover")
+.addEventListener("click",function(event){
+event.preventDefault();  
+window.location.href = "./question.html"
+})
+
+
